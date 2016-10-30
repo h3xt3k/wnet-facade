baa6b658dd1e8971b89233b23b22067e7ef51aa4
h3xt3k <jojoewig@web.de>
2016-10-30 19:27:04 +0100

    0.1

---
+++ src/main/java/boot/WNet.java
@@ -0,0 +1,17 @@
+package boot;
+
+/**
+ * Created by test1 on 29.10.16.
+ */
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WNet {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WNet.class, args);
+    }
+
+}
