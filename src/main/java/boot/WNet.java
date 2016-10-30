b9955b1267d22c246dca6ad1793d4b6c86f19f09
h3xt3k <jojoewig@web.de>
2016-10-30 20:12:49 +0100

    Test3

--- src/main/java/boot/WNet.java
+++ src/main/java/boot/WNet.java
@@ -12,7 +12,7 @@ public class WNet {
 
     public static void main(String[] args) {
         SpringApplication.run(WNet.class, args);
-        System.out.println("asdfasdfasdf");
+        System.out.println("fsda");
     }
 
 }
