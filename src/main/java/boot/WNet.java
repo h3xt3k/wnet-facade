39aa16f0843b179e0cf86a341aca2c4a249f47e7
h3xt3k <jojoewig@web.de>
2016-10-30 20:27:49 +0100

    Test6

--- src/main/java/boot/WNet.java
+++ src/main/java/boot/WNet.java
@@ -12,7 +12,7 @@ public class WNet {
 
     public static void main(String[] args) {
         SpringApplication.run(WNet.class, args);
-        System.out.println(".cvbxcvbxcvbxcvbxcvb");
+        System.out.println("klöälköäklöä");
     }
 
 }
