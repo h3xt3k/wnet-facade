3fa6dcb2f4f6da07d5cb0f53b24515b16263d6a1
h3xt3k <jojoewig@web.de>
2016-10-30 21:37:16 +0100

    Test9

--- src/main/java/boot/WNet.java
+++ src/main/java/boot/WNet.java
@@ -11,7 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WNet {
 
     public static void main(String[] args) {
-        System.out.println("187");
+        System.out.println("asdfasdfasdfsadf");
         SpringApplication.run(WNet.class, args);
     }
 
